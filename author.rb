9a9a0fdd06585dee96887cf9c1c68bb7a180984b
elbincodes <elbincodes@gmail.com>
2017-09-08 11:11:33 -0400

    I hope I got some of this right

--- author.rb
+++ author.rb
@@ -1,1 +1,40 @@
+class Author
 
+attr_accessor :name
+  @@all_authors = []
+# our Authos class has an array that hold all the intances after it is created below
+
+  def initialize (name)
+    @name = name
+    @@all_authors.push(self)
+  #when an instance is made with its name, we push that intance to the array
+  end
+
+  def self.all
+    @@all_authors
+    #this methos lets us access the array that belongs to the class, this array has all the instances
+  end
+
+  def books
+      book.all.map do |each_book|
+        if each_book.author == self.name
+          each_book
+        else
+        end
+      end
+      #this method makes use of the books Class (in other file), we go through the array that the class hold and look for a all the books that have the author mathces our instances name
+  end
+
+  def write_book (title, word_count)
+    Book.new(self.name, title, word_count)
+    #here we let this author instance  make his own book by making use of the book class, self.name is this instances name
+  end
+
+  def self.most_words
+    sorted = book.all.sort_by {|x| x.word_count}
+    sorted.last
+    
+  end
+
+
+end
--- book.rb
+++ book.rb
@@ -1,1 +1,22 @@
+class Book
 
+attr_accessor :author, :title, :word_count
+#above methods can be read and written
+  @@all_books = []
+#this is the array that all the book instances get pushed to after being made/intitalized
+  def initialize(author, title, word_count)
+    @author = author
+    @title = title
+    @word_count = word_count
+    @@all_books.push(self)
+
+    # where when a book is made with Book.new, we pass it a name, title, and word count, all of which it can later access, the instanace is then pushed into an array that the class holds
+  end
+
+def self.all
+  @@all_books
+  #this method is created so that we can access the array that the class has, array is an all the instances of the class
+end
+
+
+end
